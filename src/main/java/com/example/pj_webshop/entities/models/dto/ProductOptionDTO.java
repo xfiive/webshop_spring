16617fadd76d84e8acfc338acd9be467e27890e9
Mikhail Shytsko <xfiive@mail.ru>
2024-07-30 01:04:27 +0300

    fixed: allowed some json properties of entities to be null-equal, fixed id attachment process to dto's in addNew method in ProductPropertiesService

--- src/main/java/com/example/pj_webshop/entities/models/dto/ProductOptionDTO.java
+++ src/main/java/com/example/pj_webshop/entities/models/dto/ProductOptionDTO.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 @Data
 public class ProductOptionDTO {
     private int productOptionId;
-    private int productOptionGroupId;
+    private int groupId;
     private String name;
     private String image;
     private BigDecimal price;
