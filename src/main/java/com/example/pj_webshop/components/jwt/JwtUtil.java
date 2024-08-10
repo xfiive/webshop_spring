6051d47454fd0d2341a387bf5db59fae476628af
Mikhail Shytsko <xfiive@mail.ru>
2024-08-10 12:00:12 +0300

    fixed::changed the way of jwt generation

--- src/main/java/com/example/pj_webshop/components/jwt/JwtUtil.java
+++ src/main/java/com/example/pj_webshop/components/jwt/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -14,7 +15,8 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "secret";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
